351112ef011db7648e9771efc39db4d8ea434e26
Jay <jayrimkim@naver.com>
2022-04-06 20:30:01 +0900

    220406/ naver login

--- src/main/java/com/knit/springboot/web/dto/IndexController.java
+++ src/main/java/com/knit/springboot/web/dto/IndexController.java
@@ -1,5 +1,6 @@
 package com.knit.springboot.web.dto;
 
+import com.knit.springboot.config.auth.LoginUser;
 import com.knit.springboot.config.auth.dto.SessionUser;
 import com.knit.springboot.domain.posts.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -18,9 +19,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model,@LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
-        SessionUser user=(SessionUser) httpSession.getAttribute("user");
+//        SessionUser user=(SessionUser) httpSession.getAttribute("user");
         if (user!=null){
             model.addAttribute("userName",user.getName());
         }
