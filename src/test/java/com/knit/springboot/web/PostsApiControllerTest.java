687d935f7abb4dd970be08d4755448217ae99557
Jay <jayrimkim@naver.com>
2022-05-21 16:49:31 +0900

    220521/security setting_modified

--- src/test/java/com/knit/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/knit/springboot/web/PostsApiControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
